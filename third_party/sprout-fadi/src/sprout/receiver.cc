da16dd2033de45134678477d29117693f980b958
Fadi-B <f.barazi@sms.ed.ac.uk>
2023-03-19 08:20:59 +0000

    Receiver file was for some reason empty

---
+++ third_party/sprout-fadi/src/sprout/receiver.cc
@@ -0,0 +1,290 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <chrono>
+
+#include "receiver.hh"
+#include "sproutmath.pb.h"
+
+#include "network.h"
+
+#include "kalman_filter.hh"
+
+#include <Eigen/Dense>
+
+#include <iostream>
+
+
+using namespace std;
+
+Receiver::Receiver()
+  : _process( MAX_ARRIVAL_RATE,
+	      BROWNIAN_MOTION_RATE,
+	      OUTAGE_ESCAPE_RATE,
+	      NUM_BINS ),
+    _forecastr(),
+    _time( 0 ),
+    _score_time( -1 ),
+    _count_this_tick( 0 ),
+    _cached_forecast(),
+    _recv_queue(),
+    _ewma_rate_estimate( 1 ),
+    _collect_time( 0 ),
+    _start_time_point( chrono::high_resolution_clock::now() ),
+    _MIN_RTT( 1000000 ), /* Initialize to high value */
+    _prev_arrival( -1 ),
+    _collector_manager(TICK_LENGTH),
+    _KFforecaster(0.186, 0, 0, 0)
+{
+
+/* if (__cplusplus == 202101L) std::cerr << "C++23";
+    else if (__cplusplus == 202002L) std::cerr << "C++20";
+    else if (__cplusplus == 201703L) std::cerr << "C++17";
+    else if (__cplusplus == 201402L) std::cerr << "C++14";
+    else if (__cplusplus == 201103L) std::cerr << "C++11";
+    else if (__cplusplus == 199711L) std::cerr << "C++98";
+    else std::cerr << "pre-standard C++." << __cplusplus;
+    std::cerr << "\n";
+*/
+
+  double cur = CollectorManager::getCurrentTime(_start_time_point);
+  _collect_time = cur + TICK_LENGTH;
+
+  fprintf(stderr, "Start Time: %f \n", cur);
+  fprintf(stderr, "Collect Time: %f \n", _collect_time);
+
+  
+
+  for ( int i = 0; i < NUM_TICKS; i++ ) {
+    ProcessForecastInterval one_forecast( .001 * TICK_LENGTH,
+					  _process,
+					  MAX_ARRIVALS_PER_TICK,
+					  i + 1 );
+    _forecastr.push_back( one_forecast );
+  }
+}
+
+
+void Receiver::advance_to( const uint64_t time, bool server )
+{
+  assert( time >= _time );
+
+  while ( _time + TICK_LENGTH < time ) {
+
+    if ( server )
+    {
+      _KFforecaster.forecast(1); //Works
+    }
+
+
+    if ( (_time >= _score_time) || (_count_this_tick > 0) ) {
+
+      if ( server )
+      {
+
+       _collector_manager.resetAllHelperData();
+
+       /* Currently 1 x 5 */ //Works
+//       fprintf(stderr, "score time - measurement \n");
+       CollectorManager::Matrix measurement = _collector_manager.getCongestionSignalsHistory(3,false);
+
+//      Eigen::IOFormat CleanFmt(4, 0, ", ", "\n", "[", "]");
+//      std::cerr << "\n MEASU \n" << measurement.format(CleanFmt) << "\n";
+
+//      int rounded_bytes = int( _count_this_tick + 0.5 );
+
+//      if ( _count_this_tick > 0 && _count_this_tick < 1)
+//      {
+
+//        rounded_bytes = 1;
+
+        /* Ensure we update the rate estimation in this case*/ //This is the broken one
+	//double f = PacketCollector::to_bits_per_sec(rounded_bytes, _collector_manager.getTickTime());
+//        measurement(0, KF::iBand) = PacketCollector::to_bits_per_sec(rounded_bytes, _collector_manager.getTickTime());
+
+//      }
+	//fprintf(stderr, "Meas. Recv: %f \n", (60*1000*measurement(0, KF::iBand))/(1500*8));
+      _KFforecaster.correctForecast(measurement);
+
+      const double alpha = 1.0/4.0;
+      _ewma_rate_estimate = (1 - alpha) * _ewma_rate_estimate + ( alpha * _count_this_tick );
+
+      //For debuggin - gonna override
+//      fprintf(stderr, "MY Estimate: %f \n", (20*1000*_KFforecaster.getState().mean()(KF::iBand))/(1400*8));
+//      fprintf(stderr, "EWMA Estimate: %f \n", _ewma_rate_estimate);
+//      _KFforecaster.getState().mean()(KF::iBand) = 2;
+//      std::cerr << "\n EWMA PROP \n" << _KFforecaster.getState().mean().format(CleanFmt) << "\n";
+
+      _count_this_tick = 0;
+
+      }
+
+    } else {
+      //      fprintf( stderr, "-SKIP-" );
+    }
+    _time += TICK_LENGTH;
+  }
+
+  _KFforecaster.clearForecast();
+
+}
+
+void Receiver::recv( const uint64_t seq, const uint16_t throwaway_window, const uint16_t time_to_next, const size_t len, uint16_t timestamp, uint16_t timestamp_reception, bool server )
+{
+  _count_this_tick += len / 1400.0;
+  _recv_queue.recv( seq, throwaway_window, len );
+  _score_time = std::max( _time + time_to_next, _score_time );
+
+  //fprintf(stderr, "Receiving \n");
+
+  if ( server )
+  {
+
+  //      fprintf(stderr, "COUNT: %f \n", _count_this_tick);
+
+  	uint16_t RTT = Network::timestamp_diff(timestamp_reception, timestamp);
+  	uint16_t inter_arrival_time = Network::timestamp_diff(timestamp_reception, _prev_arrival);
+ 	 /* Check if starting value since we need at least two points */
+  	if (_prev_arrival == -1)
+  	{
+
+    	_prev_arrival = timestamp_reception;
+
+    	/* To indicate that we have not obtained two samples yet */
+    	inter_arrival_time = -1;
+
+  	}
+
+  	/* Ensure we update the previous estimate of packet arrival */
+  	_prev_arrival = timestamp_reception;
+
+  	/* Updates the minimum RTT observed during recent connection */
+  	if (RTT < _MIN_RTT)
+  	{
+
+    	_MIN_RTT = RTT;
+
+  	}
+
+        double count = ((double) len) / 1400.0;
+
+	_collector_manager.collectData(count, RTT, timestamp_reception, _MIN_RTT, inter_arrival_time);
+//	fprintf(stderr, "Collected \n");
+
+  }
+
+  //  _collector_manager.collectData(len/1400, RTT, timestamp_reception, _MIN_RTT, inter_arrival_time);
+
+
+}
+
+Sprout::DeliveryForecast Receiver::forecast( bool server )
+{
+  if ( _cached_forecast.time() == _time ) {
+    return _cached_forecast;
+  } else {
+    std::vector< int > counts;
+
+    _process.normalize();
+
+    _cached_forecast.set_received_or_lost_count( _recv_queue.packet_count() );
+    _cached_forecast.set_time( _time );
+    _cached_forecast.clear_counts();
+
+    _KFforecaster.clearForecast();
+
+    _KFforecaster.forecast(NUM_TICKS);
+
+    int tick_number = 1;
+
+    double * fadi =  _KFforecaster.getBytesToBeDrained();
+
+    //fprintf(stderr, "My Forecaster Size: %d \n", _KFforecaster.getBytesToBeDrained().size());
+
+//    fprintf(stderr, "Start\n");
+//    fprintf(stderr, "EWMA: %f \n", _ewma_rate_estimate);
+//    fprintf(stderr, "My Estimate: %f \n", fadi[0]);
+    int tick = 0;
+
+
+//    if (server) {fprintf(stderr, "SERVER: \n");} else {fprintf(stderr, "CLIENT: \n");}
+
+    for ( auto it = _forecastr.begin(); it != _forecastr.end(); it++ ) {
+      //_cached_forecast.add_counts( it->lower_quantile(_process, 0.05) );
+      //fprintf(stderr, "My Forecaster Size: %d \n", _KFforecaster.getBytesToBeDrained().size());
+      //if (iter == _KFforecaster.getBytesToBeDrained().end()) {fprintf(stderr, "Mugi \n");}
+      //fprintf(stderr, "Tick: %d \n", tick);
+      double expected_drainage = fadi[tick];
+
+      //Note: For now we have not added any uncertainty bounds
+      if ( server )
+      {
+
+//      if (!(_ewma_rate_estimate > 0)) {expected_drainage=0;}
+//      fprintf(stderr, "EWMA: %f \n", _ewma_rate_estimate*tick_number);
+//      fprintf(stderr, "Drain: %f \n", expected_drainage);
+
+//      CollectorManager::Matrix measurement = _collector_manager.getCongestionSignalsHistory(2);
+//      Eigen::IOFormat CleanFmt(4, 0, ", ", "\n", "[", "]");
+//      std::cerr << "\n History \n" << measurement.format(CleanFmt) << "\n";
+//      double bw = (20*1000 * measurement(0,0))/(1400*8);
+//      fprintf(stderr, "BW Meas: %f \n", bw);
+
+   //   if (_ewma_rate_estimate <= 0) {expected_drainage = 0;}
+
+      _cached_forecast.add_counts( /*_ewma_rate_estimate * tick_number*/ expected_drainage);
+
+      }
+      else
+      {
+
+      _cached_forecast.add_counts( 0/*std::max(0, expected_drainage*/ );
+
+      }
+      tick++;
+      tick_number++;
+
+    }
+
+    fprintf(stderr, "END - Clearing Forecast \n");
+
+    /* Ensure we clear our previous forecast - Will be irrelevant for the future */
+    _KFforecaster.clearForecast();
+
+    return _cached_forecast;
+  }
+}
+
+void Receiver::RecvQueue::recv( const uint64_t seq, const uint16_t throwaway_window, const int len )
+{
+  received_sequence_numbers.push( PacketLen( seq, len ) );
+  throwaway_before = std::max( throwaway_before, seq - throwaway_window );
+}
+
+uint64_t Receiver::RecvQueue::packet_count( void )
+{
+  /* returns cumulative count of bytes received or lost */
+  while ( !received_sequence_numbers.empty() ) {
+    if ( received_sequence_numbers.top().seq < throwaway_before ) {
+      received_sequence_numbers.pop();
+    } else {
+      break;
+    }
+  }
+
+  std::priority_queue< PacketLen, std::deque<PacketLen>, PacketLen > copy( received_sequence_numbers );
+
+  int buffer_sum = 0;
+  while ( !copy.empty() ) {
+    buffer_sum += copy.top().len;
+    copy.pop();
+  }
+
+  return throwaway_before + buffer_sum;
+}
+
